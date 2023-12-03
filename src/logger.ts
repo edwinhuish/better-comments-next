5720c50c4ec0e982b5d40f73ffc21a0d56d37a35
edwinhuish <edwinhuish@gmail.com>
2023-12-04 00:07:35 +0800

    Change class to functional

--- src/logger.ts
+++ src/logger.ts
@@ -1,28 +1,8 @@
 import * as vscode from 'vscode';
 
-export class Logger {
-  private readonly output: vscode.OutputChannel;
-
-  /**
-   * Creates a new instance of the Parser class
-   */
-  public constructor() {
-    this.output = vscode.window.createOutputChannel('Better Comments');
-  }
-
-  public log(...args: any[]) {
-    const line = args.map(obj => typeof obj === 'object' ? JSON.stringify(obj) : obj).join(' ');
-
-    this.output.appendLine(`Better Comments: ${line}`);
-  }
-}
-
-let logger: Logger;
+const _channel = vscode.window.createOutputChannel('Better Comments');
 
 export function log(...args: any[]) {
-  if (!logger) {
-    logger = new Logger();
-  }
-
-  logger.log(...args);
+  const line = args.map(obj => typeof obj === 'object' ? JSON.stringify(obj) : obj).join(' ');
+  _channel.appendLine(line);
 }
