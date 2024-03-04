600d37a94dfe7ed101d8af1f23d21d8451c5d093
edwinhuish <edwinhuish@gmail.com>
2024-03-04 10:52:39 +0800

    marge code

--- src/languages/lang/default.ts
+++ src/languages/lang/default.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import { parse as json5Parse } from 'json5';
-import { getBaseCommentRule } from '../base';
 
 export interface AvailableComments {
   lineComments: string[];
@@ -79,7 +78,7 @@ export class Language {
       return config.comments;
     }
 
-    return getBaseCommentRule(this.id) || {};
+    return getDefaultComments(this.id) || {};
   }
 
   /**
@@ -118,5 +117,117 @@ export class Language {
   setAvailableComments(comments: AvailableComments) {
     this.availableComments = comments;
     return this;
+  }
+}
+
+function getDefaultComments(languageCode: string): vscode.CommentRule | undefined {
+  switch (languageCode) {
+    case 'asciidoc':
+      return ({ lineComment: '//', blockComment: ['////', '////'] });
+    case 'apex':
+    case 'javascript':
+    case 'javascriptreact':
+    case 'typescript':
+    case 'typescriptreact':
+    case 'al':
+    case 'c':
+    case 'cpp':
+    case 'csharp':
+    case 'dart':
+    case 'flax':
+    case 'fsharp':
+    case 'go':
+    case 'groovy':
+    case 'haxe':
+    case 'java':
+    case 'jsonc':
+    case 'kotlin':
+    case 'less':
+    case 'pascal':
+    case 'objectpascal':
+    case 'php':
+    case 'rust':
+    case 'scala':
+    case 'sass':
+    case 'scss':
+    case 'stylus':
+    case 'swift':
+    case 'verilog':
+      return ({ lineComment: '//', blockComment: ['/*', '*/'] });
+    case 'css':
+      return ({ blockComment: ['/*', '*/'] });
+    case 'coffeescript':
+    case 'dockerfile':
+    case 'gdscript':
+    case 'graphql':
+    case 'julia':
+    case 'makefile':
+    case 'perl':
+    case 'perl6':
+    case 'puppet':
+    case 'r':
+    case 'ruby':
+    case 'shellscript':
+    case 'tcl':
+    case 'yaml':
+      return ({ lineComment: '#' });
+    case 'elixir':
+    case 'python':
+      return ({ lineComment: '#', blockComment: ['"""', '"""'] });
+    case 'nim':
+      return ({ lineComment: '#', blockComment: ['#[', ']#'] });
+    case 'powershell':
+      return ({ lineComment: '#', blockComment: ['<#', '#>'] });
+    case 'ada':
+    case 'hive-sql':
+    case 'pig':
+    case 'plsql':
+    case 'sql':
+      return ({ lineComment: '--' });
+    case 'lua':
+      return ({ lineComment: '--', blockComment: ['--[[', ']]'] });
+    case 'elm':
+    case 'haskell':
+      return ({ lineComment: '--', blockComment: ['{-', '-}'] });
+    case 'vb':
+    case 'asp':
+    case 'diagram': // ? PlantUML is recognized as Diagram (diagram)
+      return ({ lineComment: '\'' });
+    case 'bibtex':
+    case 'erlang':
+    case 'latex':
+    case 'matlab':
+      return ({ lineComment: '%' });
+    case 'clojure':
+    case 'elps':
+    case 'racket':
+    case 'lisp':
+      return ({ lineComment: ';' });
+    case 'terraform':
+      return ({ lineComment: '#', blockComment: ['/*', '*/'] });
+    case 'COBOL':
+      return ({ lineComment: '*>' });
+    case 'fortran-modern':
+      return ({ lineComment: 'c' });
+    case 'SAS':
+    case 'stata':
+      return ({ lineComment: '*', blockComment: ['/*', '*/'] });
+    case 'html':
+    case 'xml':
+    case 'markdown':
+    case 'vue':
+      return ({ blockComment: ['<!--', '-->'] });
+    case 'twig':
+      return ({ blockComment: ['{#', '#}'] });
+    case 'genstat':
+      return ({ lineComment: '\\', blockComment: ['"', '"'] });
+    case 'cfml':
+      return ({ blockComment: ['<!---', '--->'] });
+    case 'shaderlab':
+      return ({ lineComment: '//' });
+    case 'razor':
+      return ({ blockComment: ['@*', '*@'] });
+    default:
+      return undefined;
   }
 }
