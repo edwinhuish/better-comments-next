966876eda01851fca3fb8188f5c485f6434da143
Edwin Xu <edwinhuish@gmail.com>
2024-12-06 22:49:43 +0800

    chore: add ruby sample

---
+++ samples/ruby.rb
@@ -0,0 +1,14 @@
+# ! this is a alert
+def say_hello
+  # # print to stdout
+  puts "Hello, World!"
+end
+
+# # call function
+say_hello
+  
+#
+# todo: multi-line in block comment example
+#       it will break until empty line
+#       or final line of the comment
+#
