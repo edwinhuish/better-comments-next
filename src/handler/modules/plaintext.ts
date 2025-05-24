937a118d438468810b835925fc7c3a366f5fc713
edwinhuish <edwinhuish@gmail.com>
2025-05-25 02:02:58 +0800

    fix: plaintext configuration

--- src/handler/modules/plaintext.ts
+++ src/handler/modules/plaintext.ts
@@ -1,8 +1,13 @@
 import type { BlockCommentSlice, PickParams } from './common';
+import * as configuration from '@/configuration';
 import { CommonHandler } from './common';
 
 export class PlainTextHandler extends CommonHandler {
   protected async pickBlockCommentSlices(params: PickParams): Promise<Array<BlockCommentSlice>> {
+    if (!configuration.getConfigurationFlatten().highlightPlainText) {
+      return [];
+    }
+
     return [{
       start: params.offset,
       end: params.offset + params.text.length,
