388dc36eb5d944bf09965f70c9cac65cc04d102c
edwinhuish <edwinhuish@gmail.com>
2025-05-25 02:09:41 +0800

    fix: react embed comment

--- src/handler/handler.ts
+++ src/handler/handler.ts
@@ -2,12 +2,16 @@ import type { Handler, UpdateParams } from './modules/common';
 import * as configuration from '../configuration';
 import { CommonHandler } from './modules/common';
 import { PlainTextHandler } from './modules/plaintext';
+import { ReactHandler } from './modules/react';
 import { ShellscriptHandler } from './modules/shellscript';
 
 const cached = new Map<string, Handler>();
 
 function newHandler(languageId: string): Handler {
   switch (languageId) {
+    case 'javascriptreact':
+    case 'typescriptreact':
+      return new ReactHandler(languageId);
     case 'shellscript':
       return new ShellscriptHandler(languageId);
     case 'plaintext':
