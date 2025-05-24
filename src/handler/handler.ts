67d5355e9a58e9466eee9d7cc5c13e9acfb0d9e7
edwinhuish <edwinhuish@gmail.com>
2025-05-25 01:31:01 +0800

    fix: #46 extra shellscript comment check

--- src/handler/handler.ts
+++ src/handler/handler.ts
@@ -1,12 +1,15 @@
-import type { Handler, UpdateOptions } from './modules/common';
+import type { Handler, UpdateParams } from './modules/common';
 import * as configuration from '../configuration';
 import { CommonHandler } from './modules/common';
 import { PlainTextHandler } from './modules/plaintext';
+import { ShellscriptHandler } from './modules/shellscript';
 
 const cached = new Map<string, Handler>();
 
 function newHandler(languageId: string): Handler {
   switch (languageId) {
+    case 'shellscript':
+      return new ShellscriptHandler(languageId);
     case 'plaintext':
       return new PlainTextHandler(languageId);
     default:
@@ -25,7 +28,7 @@ function useHandler(languageId: string): Handler {
   return handler;
 }
 
-export function triggerUpdateDecorations(options: UpdateOptions) {
+export function triggerUpdateDecorations(params: UpdateParams) {
   const configuratgion = configuration.getConfigurationFlatten();
-  return useHandler(options.editor.document.languageId).triggerUpdateDecorations({ ...options, timeout: configuratgion.updateDelay });
+  return useHandler(params.editor.document.languageId).triggerUpdateDecorations({ ...params, timeout: configuratgion.updateDelay });
 }
