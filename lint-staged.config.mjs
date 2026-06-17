4fc36a9782ea61aa8a4b01ccb94e00b8148dbfaf
edwinhuish <edwinhuish@gmail.com>
2026-06-17 11:57:46 +0800

    chore(lint-staged): 扩展检查文件范围

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,10 @@
+const eslint = 'eslint --cache --max-warnings 0';
+const tsc = 'tsc -p . --noEmit';
+
 export default {
-  '*.{js,jsx,ts,tsx,vue}': () => {
-    return [
-      'tsc -p . --noEmit',
-      'eslint --cache --max-warnings 0 ',
-    ];
-  },
+  '*.{js,jsx,ts,tsx,vue}': [tsc, eslint],
+  '*.{css,less,scss,sass}': [eslint],
+  '*.{md,markdown}': [eslint],
+  '*.{json,json5}': [eslint],
+  '*.{yml,yaml}': [eslint],
 };
